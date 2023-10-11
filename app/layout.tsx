cf8937e363b2a64b4ca9616b734a925c92d843b4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-11 15:54:24 +0300

    import navbar, footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
-// import Footer from "@/components/Footer";
-// import Navbar from "@/components/Navbar";
+import { Navbar, Footer } from '@/components';
 
 export const metadata = {
   title: 'Quibbledrib',
@@ -11,9 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
-        {/* <Navbar /> */}
+        <Navbar />
         <main>{children}</main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
