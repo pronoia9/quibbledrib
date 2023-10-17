b0dcadfe22ce562cf128a2f3fc5ec2489ef2f820
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-17 17:19:55 +0300

    add init navbar logo and links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { NavLinks } from "@/constant";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image src='/logo.svg' width={116} height={43} alt='logo' />
+        </Link>
+
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link, index) => (
+            <Link key={`navlinks-${index}-${link.text}`} href={link.href}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
