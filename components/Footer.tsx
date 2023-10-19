87e74e4cdc8a2500197e2e3598874d693e19bdee
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-19 17:00:15 +0300

    add footer columns

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,7 @@
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { footerLinks } from '@/constant';
 
 const Footer = () => {
   return (
@@ -11,8 +14,39 @@ const Footer = () => {
             Quibbledrib is the world&apos;s leading community for creatives to share, grow, and get hired.
           </p>
         </div>
+
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
+          <div className='flex-1 flex flex-col gap-4'>
+            <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+
+          <div className='flex-1 flex flex-col gap-4'>
+            <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+            <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links} />
+        </div>
       </div>
     </footer>
   );
 };
 export default Footer;
+
+const FooterColumn = ({ title, links }: { title: string; links: Array<string> }) => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>{title}</h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      {links.map((link) => (
+        <Link href='/' key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
