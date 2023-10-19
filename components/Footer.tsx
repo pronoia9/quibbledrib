43c5c56707e67ead7512d90d27f5e4b188cd546d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-19 16:58:16 +0300

    add logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,18 @@
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image src='/logo-purple.svg' width={116} height={38} alt='logo' />
+
+          <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+            Quibbledrib is the world&apos;s leading community for creatives to share, grow, and get hired.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
