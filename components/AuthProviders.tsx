348cdc6c804208f91bf85bc6fa8dd737b4560816
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-21 17:24:04 +0300

    add init authproviders state

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,29 @@
+'use client';
+
+import { getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
+
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  if (providers)
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, index) => (
+          <button key={`authproviders-providers-${index}`}>{provider.id}</button>
+        ))}
+      </div>
+    );
 };
 export default AuthProviders;
