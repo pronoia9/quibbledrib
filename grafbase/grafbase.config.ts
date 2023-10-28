60e4c2c3ef525fd1da2c900861070e889ff65483
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-10-28 17:35:34 +0300

    add auth to user and project models

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,27 +1,37 @@
 import { g, auth, config } from '@grafbase/sdk';
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 30 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g
-    .relation(() => Project)
-    .list()
-    .optional(),
-});
+// @ts-ignore
+const User = g
+  .model('User', {
+    name: g.string().length({ min: 2, max: 30 }),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().optional(),
+    githubUrl: g.url().optional(),
+    linkedInUrl: g.url().optional(),
+    projects: g
+      .relation(() => Project)
+      .list()
+      .optional(),
+  })
+  .auth((rules) => {
+    rules.public().read();
+  });
 
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-});
+// @ts-ignore
+const Project = g
+  .model('Project', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read(), rules.private().create().delete().update();
+  });
 
 const jwt = auth.JWT({
   issuer: 'grafbase',
